da295acd011c1b4a5b826a7a2aa17a48fbbadb72
yearsky <yearskylifael@Yearskys-MacBook-Pro.local>
2022-03-23 12:16:53 +0700

    dev-react v1

---
+++ src/navigation/horizontal/dashboards.js
@@ -0,0 +1,19 @@
+// ** Icons Import
+import { Home, Activity, ShoppingCart } from 'react-feather'
+
+export default [
+  {
+    id: 'dashboards',
+    title: 'Dashboards',
+    icon: <Home />,
+    children: [
+      {
+        id: 'analyticsDash',
+        title: 'Analytics',
+        icon: <Activity />,
+        navLink: '/dashboard/analytics'
+      }
+     
+    ]
+  }
+]
---
+++ src/navigation/vertical/dashboards.js
@@ -0,0 +1,20 @@
+// ** Icons Import
+import { Home, Circle } from 'react-feather'
+
+export default [
+  {
+    id: 'dashboards',
+    title: 'Dashboards',
+    icon: <Home size={20} />,
+    badge: 'light-warning',
+    children: [
+      {
+        id: 'analyticsDash',
+        title: 'Analytics',
+        icon: <Circle size={12} />,
+        navLink: '/dashboard/analytics'
+      }
+     
+    ]
+  }
+]
